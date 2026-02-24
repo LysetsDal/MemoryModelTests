729dce78580d5fb7e01046feb9da74b3e5f8e1df
LysetsDal <asly@itu.dk>
2026-02-24 20:25:52 +0100

    Readonly tests

--- MemoryModelTests/Program.cs
+++ MemoryModelTests/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using MemoryModelTests.Readonly;
+using Xunit.Abstractions;
 
-Console.WriteLine("Hello, World!");
+namespace MemoryModelTests;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var readonlyImmutable = new ReadonlyImmutable();
+        // readonlyImmutable.Test_Readonly_Publication_Safety();
+        readonlyImmutable.Test_Readonly_Store_Load_Semantics();
+    }
+}
--- MemoryModelTests/Readonly/ReadonlyImmutable.cs
+++ MemoryModelTests/Readonly/ReadonlyImmutable.cs
@@ -1,22 +1,17 @@
 using Xunit;
 using Xunit.Abstractions;
-// ReSharper disable ConvertConstructorToMemberInitializers
-// ReSharper disable ConvertToPrimaryConstructor
-
 
 namespace MemoryModelTests.Readonly;
 
-public class ReadonlyImmutable(ITestOutputHelper testOutputHelper)
+public class ReadonlyImmutable()
 {
-    private readonly ITestOutputHelper _testOutputHelper = testOutputHelper;
-
     [Fact]
     public void Test_Readonly_Publication_Safety()
     {
         for (int i = 0; i < 100_000; i++)
         {
             HelperObject helperObject = null;
-            int observedData = 0;
+            int observedData = -1;
             
             var startSignal = new ManualResetEventSlim(false);
 
@@ -24,7 +19,8 @@ public class ReadonlyImmutable(ITestOutputHelper testOutputHelper)
             Thread t1 = new Thread(() =>
             {
                 startSignal.Wait();
-                helperObject = new HelperObject(1);
+                var obj = new HelperObject(1);
+                helperObject = obj;
             });
 
             // Thread 2: Reads the field after object construction finishes
@@ -48,11 +44,62 @@ public class ReadonlyImmutable(ITestOutputHelper testOutputHelper)
             Assert.Equal(1, observedData);
         }
     }
+    
+    [Fact]
+    public void Test_Readonly_Store_Load_Semantics()
+    {
+        for (int i = 0; i < 100_000; i++)
+        {
+            HelperObject helperObject1 = null;
+            HelperObject helperObject2 = null;
+            int observedData1 = -1;
+            int observedData2 = -1;
+            
+            var startSignal = new ManualResetEventSlim(false);
 
+            // Thread 1: Constructs the class with the readonly field
+            Thread t1 = new Thread(() =>
+            {
+                startSignal.Wait();
+                helperObject1 = new HelperObject(1);
+                
+                while(helperObject2 == null)
+                {
+                    Thread.SpinWait(1);
+                }
+                
+                observedData1 = helperObject2.getData();
+            });
+
+            // Thread 2: Reads the field after object construction finishes
+            Thread t2 = new Thread(() =>
+            {
+                startSignal.Wait();
+                helperObject2 = new HelperObject(2);
+                
+                while(helperObject1 == null)
+                {
+                    Thread.SpinWait(1);
+                }
+                
+                observedData2 = helperObject1.getData();
+
+            });
+
+            t1.Start(); t2.Start();
+            startSignal.Set(); // Release both threads at once
+            t1.Join(); t2.Join();
+
+            // If readonly semantics failed, observedData should be 0
+            Assert.Equal(2, observedData1);
+            Assert.Equal(1, observedData2);
+        }
+    }
+    
 
     private class HelperObject
     {
-        private readonly int _data;
+        private int _data;
             
         public HelperObject(int data)
         {
